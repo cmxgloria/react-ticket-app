eebcfc1ed94e1e9c65ec97f01f66a667f6ec1747
Gloria Chen <cmxgloria@gmail.com>
2019-06-05 22:46:09 +1000

    changed header

--- src/App.js
+++ src/App.js
@@ -2,25 +2,15 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  getTicket = async (e) => {
-    const ticketName = e.target.elements.recipeName.value
-    e.preventDefault();
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://meixiao.zendesk.com/api/search?Key=${API_KEY}&q=ticket&count=25`);
-
-    const data = await api_call.json();
-    console.log(data);
-
+  state = {
+    recipes: []
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src="https://d1eipm3vz40hy0.cloudfront.net/images/p-omnichannel/logo-suite-z.svg" alt="logo" />
-          <a
-            href="https://www.zendesk.com/"
-            target="_blank"
-          >
-            Zendesk
+          <a href="https://www.zendesk.com/" target="_blank">
+            <img src="https://d1eipm3vz40hy0.cloudfront.net/images/p-omnichannel/logo-suite-z.svg" alt="logo" />
           </a>
         </header>
       </div>
