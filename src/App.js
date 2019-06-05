81ade260e06461b6051d0eb20f082bb78e72811d
Gloria Chen <cmxgloria@gmail.com>
2019-06-05 12:40:28 +1000

    change subject

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Logo} className="App-logo" alt="logo" />
+        <logo />;
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Zendesk Ticket Viewer
         </a>
       </header>
     </div>
