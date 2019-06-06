5b8cb5a6b323894cf6140d1f23eb36220c3092f7
Gloria Chen <cmxgloria@gmail.com>
2019-06-06 11:25:14 +1000

    added ticket status

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -5,16 +5,18 @@ const Tickets = props => (
     <div className="row">
       {props.tickets.map((ticket) => {
         return (
-          <div className="Ticket-container">
+          <div key={ticket.subject} className="Ticket-container">
             <div className="Ticket-box">
               <div className="Ticket-text">
                 <h4>{ticket.subject}</h4>
+                <h5>{ticket.status}</h5>
                 <div key={ticket.requester_id}>
                   <p>Created At: {new Date(ticket.created_at).toDateString()}</p>
                   <p>{ticket.description}</p>
                 </div>
               </div>
             </div>
+            <button></button>
           </div>);
       })}
     </div>
