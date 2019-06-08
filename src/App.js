44c87011da81b42c457630f041ed6310859f81c0
Gloria Chen <cmxgloria@gmail.com>
2019-06-08 19:39:08 +1000

    updated App.js css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import Tickets from './components/Tickets';
+import { Link } from 'react-router-dom';
 
 class App extends React.Component {
   state = {
-    tickets: []
+    tickets: [],
+    nextPage: null,
+    previousPage: null,
+    loading: false,
   }
-  componentDidMount = async () => {
-    const api_call = await fetch('https://cors-anywhere.herokuapp.com/https://meixiao.zendesk.com/api/v2/tickets.json?per_page=25',
+  apiCall = async (ticksUrl) => {
+    this.setState({
+      loading: true
+    });
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/${ticksUrl}`,
       {
         method: 'GET',
         headers: {
@@ -17,9 +24,26 @@ class App extends React.Component {
       });
     const data = await api_call.json();
     console.log(data);
-    this.setState({tickets: data.tickets});
+    this.setState({
+      tickets: data.tickets,
+      nextPage: data.next_page,
+      previousPage: data.previous_page,
+      loading: false
+    });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps, prevState);
+  }
+  componentDidMount = async () => {
+    console.log(this.props)
+    await this.apiCall('https://meixiao.zendesk.com/api/v2/tickets.json?per_page=25&page=1');
+    // const page = this.props.match.params.id;
   }
   render() {
+    console.log(this.props)
+    if (this.state.loading) {
+      return <div>loading ...</div>
+    }
     return (
       <div className="App">
         <header className="App-header">
@@ -28,6 +52,9 @@ class App extends React.Component {
           </a>
         </header>
         <Tickets tickets={this.state.tickets} />
+        {this.state.previousPage && <button onClick={(e) => this.apiCall(this.state.previousPage)}>prev</button>}
+        {this.state.nextPage && <button onClick={(e) => this.apiCall(this.state.nextPage)}>next</button>}
+        {this.state.nextPage && <button><Link to={{ pathname: '/?page=2', query: { page: 2 } }}>next</Link></button>}
       </div>
     );
   }
