adb6f292bd22f24c02894ea602372214ca472a74
Gloria Chen <cmxgloria@gmail.com>
2019-06-08 19:39:45 +1000

    adjusted ticket status

--- src/components/Tickets.js
+++ src/components/Tickets.js
@@ -3,30 +3,28 @@ import { Link } from 'react-router-dom';
 
 const Tickets = props => (
   <div className="Container">
-    <div className="row">
-      {props.tickets.map((ticket) => {
-        return (
-          <div key={ticket.subject} className="Ticket-container">
-            <div className="Ticket-box">
-              <div className="Ticket-text">
-                <h4>{ticket.subject}</h4>
-                <h5>{ticket.status}</h5>
-                <div key={ticket.requester_id}>
-                  <p>Created At: {new Date(ticket.created_at).toDateString()}</p>
-                  <p>Description: <span>{ticket.description}</span></p>
-                </div>
+    {props.tickets.map((ticket) => {
+      return (
+        <div key={ticket.subject} className="Ticket-container">
+          <div className="Ticket-box">
+            <div className="Ticket-text">
+              <h4>{ticket.subject}</h4>
+              <span>status: {ticket.status}</span>
+              <div key={ticket.requester_id}>
+                <p>Created At: {new Date(ticket.created_at).toDateString()}</p>
+                {/* <p>Description: <span>{ticket.description}</span></p> */}
               </div>
             </div>
-            <button className="Ticket-button">
-              <Link to={{
-                pathname: `/ticket/${ticket.id}`
-              }}>
-                View Ticket
+          </div>
+          <button className="Ticket-button">
+            <Link to={{
+              pathname: `/ticket/${ticket.id}`
+            }}>
+              View Ticket
               </Link>
-            </button>
-          </div>);
-      })}
-    </div>
+          </button>
+        </div>);
+    })}
   </div>
 );
 export default Tickets;
